f31f178d1edc6f9b77dea1124e08bdc97eced41f
Roietreine <richardrejano52296@gmail.com>
2023-10-08 20:12:44 +0800

    added activity login motion

--- app/src/main/java/com/exampl/motionlayout/MainActivity.kt
+++ app/src/main/java/com/exampl/motionlayout/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.motionlayout
 
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnimationUtils
+import android.view.animation.TranslateAnimation
+import android.widget.ImageView
 import com.exampl.motionlayout.R
 import com.exampl.motionlayout.databinding.ActivityMainBinding
 
@@ -13,5 +18,26 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val moveUpAnimation = AnimationUtils.loadAnimation(this, R.anim.move_up_animation)
+        val ivFirstCircle = binding.ivFirstCircle
+        val ivSecondCircle = binding.ivSecondCircle
+        val ivThirdCircle = binding.ivThirdCircle
+        val ivFourthCircle = binding.ivFourthCircle
+        val ivMainLogo = binding.ivLogo
+
+        val halfHeight = ivFirstCircle.height / 2 // Calculate half of the ImageView's height
+        ivFirstCircle.translationY = -halfHeight.toFloat()
+        ivFirstCircle.startAnimation(moveUpAnimation)
+
+        ivSecondCircle.translationY = -halfHeight.toFloat()
+        ivSecondCircle.startAnimation(moveUpAnimation)
+
+        ivThirdCircle.translationY = -halfHeight.toFloat()
+        ivThirdCircle.startAnimation(moveUpAnimation)
+
+        ivFourthCircle.translationY = -halfHeight.toFloat()
+        ivFourthCircle.startAnimation(moveUpAnimation)
+
     }
 }
